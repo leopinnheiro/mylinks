45a30f117074924acdc5f9b6c7f94bd375e360db
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-10-02 05:43:34 -0300

    Criando componentes base.

--- src/pages/NewLink/index.js
+++ src/pages/NewLink/index.js
@@ -1,7 +1,23 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewLink() {
   return (
-    <PageHeader title="Novo link" />
+    <>
+      <PageHeader title="Novo link" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Produtividade</option>
+        <option value="123">Acessos</option>
+      </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
+    </>
   );
 }
